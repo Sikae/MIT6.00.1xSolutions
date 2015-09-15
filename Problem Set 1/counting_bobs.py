354844ddedd92621fe92f33535add15e3e2a184f
m <lmiguelvargasf@gmail.com>
2015-09-15 01:28:07 -0500

    Solve counting bobs problem.

---
+++ Problem Set 1/counting_bobs.py
@@ -0,0 +1,27 @@
+__author__ = 'm'
+
+"""
+Assume s is a string of lower case characters.
+
+Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl',
+then your program should print
+
+Number of times bob occurs is: 2
+For problems such as these, do not include raw_input statements or define the variable s in any way. Our automating
+testing will provide a value of s for you - so the code you submit in the following box should assume s is already
+defined. If you are confused by this instruction, please review L4 Problems 10 and 11 before you begin this problem set.
+"""
+
+
+def count_bobs(word):
+    keyword = "bob"
+    keyword_length = len(keyword)
+    counter = 0
+    for index in range(len(word) - keyword_length + 1):
+        if word[index:index + keyword_length] == keyword:
+            counter += 1
+    return counter
+
+
+s = input("Enter a string: ")
+print("Number of times bob occurs is: " + str(count_bobs(s)))
