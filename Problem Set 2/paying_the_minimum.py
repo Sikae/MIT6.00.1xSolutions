b241e23638c3faa54519505b49c09b699fda5561
m <lmiguelvargasf@gmail.com>
2015-10-21 10:04:25 -0500

    Solve paying the minimum problem.

--- Problem Set 2/paying_the_minimum.py
+++ Problem Set 2/paying_the_minimum.py
@@ -29,10 +29,56 @@ Remaining balance: 4784.0
 """
 
 
-def compute_balance(unpaid_balance):
-    return unpaid_balance * (1 + 2)
+def compute_updated_balance(balance, minimum_monthly_payment, annual_interest_rate):
+    monthly_interest_rate = annual_interest_rate / 12.0
+    monthly_unpaid_balance = balance - minimum_monthly_payment
+
+    return monthly_unpaid_balance * (1 + monthly_interest_rate)
 
 
-def print_monthly_balance():
+def compute_minimum_monthly_payment(monthly_payment_rate, balance):
+    return monthly_payment_rate * balance
+
+
+def round_number_two_decimals(number):
+    return round(number, 2)
+
+
+def get_updated_balance_and_minimum_monthly_payment(balance, annual_interest_rate, monthly_payment_rate):
+    minimum_monthly_payment = compute_minimum_monthly_payment(monthly_payment_rate, balance)
+    updated_balance = compute_updated_balance(balance, minimum_monthly_payment, annual_interest_rate)
+
+    return updated_balance, minimum_monthly_payment
+
+
+def print_monthly_balance(balance, annual_interest_rate, monthly_payment_rate, month):
+    updated_balance, minimum_monthly_payment = get_updated_balance_and_minimum_monthly_payment(balance,
+                                                                                               annual_interest_rate,
+                                                                                               monthly_payment_rate)
+    print("Month: " + str(month))
+    print("Minimum monthly payment: " + str(round_number_two_decimals(minimum_monthly_payment)))
+    print("Remaining balance: " + str(round_number_two_decimals(updated_balance)))
+
+
+def print_balance_after_a_year(balance, annual_interest_rate, monthly_payment_rate):
+    total_paid = 0
     for month in range(12):
-        print("This month you have to pay " + str(compute_balance()))
+        print_monthly_balance(balance, annual_interest_rate, monthly_payment_rate, month + 1)
+        balance, monthly_payment = get_updated_balance_and_minimum_monthly_payment(balance, annual_interest_rate,
+                                                                                   monthly_payment_rate)
+        total_paid += monthly_payment
+
+    print("Total paid: " + str(round_number_two_decimals(total_paid)))
+    print("Remaining balance: " + str(round_number_two_decimals(balance)))
+
+
+def main():
+    balance = eval(input("Enter the initial balance: "))
+    annual_interest_rate = eval(input("Enter the annual interest rate as a decimal: "))
+    monthly_payment_rate = eval(input("Enter the minimum monthly payment rate as a decimal: "))
+
+    print_balance_after_a_year(balance, annual_interest_rate, monthly_payment_rate)
+
+
+if __name__ == "__main__":
+    main()
