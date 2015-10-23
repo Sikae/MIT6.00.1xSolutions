0d9a9a48c54e4ef430b77f077bd7853d74b5f785
m <lmiguelvargasf@gmail.com>
2015-10-23 08:20:16 -0500

    Improve counting_bobs docstring.

--- Problem Set 1/counting_bobs.py
+++ Problem Set 1/counting_bobs.py
@@ -1,15 +1,7 @@
 __author__ = 'm'
 
 """
-Assume s is a string of lower case characters.
-
-Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl',
-then your program should print
-
-Number of times bob occurs is: 2
-For problems such as these, do not include raw_input statements or define the variable s in any way. Our automating
-testing will provide a value of s for you - so the code you submit in the following box should assume s is already
-defined. If you are confused by this instruction, please review L4 Problems 10 and 11 before you begin this problem set.
+This program prints the number of times the string "bob" occurs in a string.
 """
 
 
