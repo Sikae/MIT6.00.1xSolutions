700bc80876516d9933f824ecaea63582a6761450
m <lmiguelvargasf@gmail.com>
2015-10-23 08:18:57 -0500

    Improve counting_vowels docstring.

--- Problem Set 1/counting_vowels.py
+++ Problem Set 1/counting_vowels.py
@@ -1,16 +1,7 @@
 __author__ = 'm'
 
 """
-Assume s is a string of lower case characters.
-
-Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o',
-and 'u'. For example, if s = 'azcbobobegghakl', your program should print:
-
-Number of vowels: 5
-
-For problems such as these, do not include input statements or define the variable s in any way. Our automated testing
-will provide a value of s for you - so the code you submit in the following box should assume s is already defined.
-If you are confused by this instruction, please review L4 Problems 10 and 11 before you begin this problem set.
+This program prints the number of vowels (lowercase) contained in a string.
 """
 
 
