528c60c00911df3136f0d1e6bb50b7f17aa0b18a
m <lmiguelvargasf@gmail.com>
2015-10-26 00:31:45 -0500

    Finish problem_5.

--- Quiz/problem_5.py
+++ Quiz/problem_5.py
@@ -1,1 +1,18 @@
 __author__ = 'm'
+
+def primes_list(N):
+    '''
+    N: an integer
+    '''
+    primes = []
+
+    for number in range(2, N + 1):
+        is_prime = True
+        for factor in range(2, number):
+            if number % factor == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(number)
+
+    return primes
