87317afaa125e1b2dd26c26e13929218b0e7736c
m <lmiguelvargasf@gmail.com>
2015-10-25 23:54:37 -0500

    Finish insert_new_lines recursive function implementation.

--- Problem Set 6/ps6_recursion.py
+++ Problem Set 6/ps6_recursion.py
@@ -5,22 +5,23 @@
 #
 # Problem 3: Recursive String Reversal
 #
+import string
 
 
-def reverse_string(string):
+def reverse_string(character_string):
     """
-    Given a string, recursively returns a reversed copy of the string.
-    For example, if the string is 'abc', the function returns 'cba'.
-    The only string operations you are allowed to use are indexing,
+    Given a character_string, recursively returns a reversed copy of the character_string.
+    For example, if the character_string is 'abc', the function returns 'cba'.
+    The only character_string operations you are allowed to use are indexing,
     slicing, and concatenation.
     
-    string: a string
-    returns: a reversed string
+    character_string: a character_string
+    returns: a reversed character_string
     """
-    if len(string) == 1:
-        return string
+    if len(character_string) == 1:
+        return character_string
 
-    return string[-1] + reverse_string(string[:-1])
+    return character_string[-1] + reverse_string(character_string[:-1])
 
 
 #
@@ -51,7 +52,7 @@ def x_ian(x, word):
 #
 # Problem 5: Typewriter
 #
-def insert_new_lines(text, lineLength):
+def insert_new_lines(text, line_length):
     """
     Given text and a desired line length, wrap the text as a typewriter would.
     Insert a newline character ("\n") after each word that reaches or exceeds
@@ -62,7 +63,18 @@ def insert_new_lines(text, lineLength):
         the next word.
     returns: a string, with newline characters inserted appropriately. 
     """
-    pass
+    if len(text) <= line_length:
+        return text
+
+    if text[line_length - 1] not in string.ascii_letters:
+        return text[:line_length] + "\n" + insert_new_lines(text[line_length:], line_length)
+
+    try:
+        last_index = line_length + text[line_length:].index(" ")
+    except ValueError:
+        last_index = len(text) - 1
+
+    return text[:last_index + 1] + "\n" + insert_new_lines(text[last_index + 1:], line_length)
 
 
 def main():
@@ -71,6 +83,16 @@ def main():
     print(x_ian('eric', 'cerium'))
     print(x_ian('john', 'mahjong'))
     print(x_ian("roberto", "crooooootttttbpppefadfdarsssst111oppp"))
+    print(insert_new_lines("Russian submarines and spy ships are aggressively operating near the vital undersea "
+                           "cables that carry almost all global Internet communications, raising concerns among some "
+                           "American military and intelligence officials that the Russians might be planning to attack "
+                           "those lines in times of tension or conflict. The issue goes beyond old worries during the "
+                           "Cold War that the Russians would tap into the cables  a task American intelligence "
+                           "agencies also mastered decades ago. The alarm today is deeper: The ultimate Russian hack "
+                           "on the United States could involve severing the fiber-optic cables at some of their "
+                           "hardest-to-access locations to halt the instant communications on which the West's "
+                           "governments, economies and citizens have grown dependent.", 40))
+
 
 if __name__ == '__main__':
     main()
