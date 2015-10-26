9ce2efac2997d5fef5d7cc61d55170dc5ddef4bf
m <lmiguelvargasf@gmail.com>
2015-10-26 07:06:23 -0500

    Finish problem 8.

--- Quiz/problem_8.py
+++ Quiz/problem_8.py
@@ -1,3 +1,35 @@
 __author__ = 'm'
 
 
+def f(s):
+    return 'a' in s
+
+
+def satisfies_f(L):
+    """
+    Assumes L is a list of strings
+    Assume function f is already defined for you and it maps a string to a Boolean
+    Mutates L such that it contains all of the strings, s, originally in L such
+            that f(s) returns True, and no other elements
+    Returns the length of L after mutation
+    """
+    bool_list = []
+
+    for e in L:
+        bool_list.append(f(e))
+
+    for i in range(len(bool_list)):
+        if not bool_list[i]:
+            L[i] = None
+
+    while True:
+        try:
+            L.remove(None)
+        except ValueError:
+            break
+
+    return len(L)
+
+L = ['a', 'b', 'a']
+print(satisfies_f(L))
+print(L)
