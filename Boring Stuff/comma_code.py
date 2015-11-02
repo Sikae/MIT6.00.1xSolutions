8e88bb762a5bbfa388726f7895e43feafbc255b2
D <laxos90@gmail.com>
2015-11-02 15:21:13 -0500

    Attempt first approach.

--- Boring Stuff/comma_code.py
+++ Boring Stuff/comma_code.py
@@ -3,5 +3,11 @@ __author__ = 'DELL'
 spam = ['apples', 'bananas', 'tofu', 'cats']
 
 def separate_items_by_commas(items):
-    pass
+    string = ""
+    for i in items:
+        string += i
+    return print(string)
+
+separate_items_by_commas(spam)
+
 
