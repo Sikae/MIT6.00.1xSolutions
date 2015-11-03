8a543e6c7262c1a49a7a4cf601bcd2739c89dd84
Miguel Vargas <mvargas@devsu.com>
2015-11-03 15:44:38 -0500

    Implement functions make_trigger and filter_stories.

--- Problem Set 7/ps7.py
+++ Problem Set 7/ps7.py
@@ -101,7 +101,7 @@ class WordTrigger(Trigger):
     def __init__(self, word):
         self.word = word.lower()
 
-    def isWordIn(self, text):
+    def is_word_in(self, text):
         text_witout_punctuation = replace_punctuation_mark_with_a_space(text.lower())
         text_word = text_witout_punctuation.split()
         for word in text_word:
@@ -112,17 +112,17 @@ class WordTrigger(Trigger):
 
 class TitleTrigger(WordTrigger):
     def evaluate(self, story):
-        return WordTrigger.isWordIn(self, story.get_title())
+        return WordTrigger.is_word_in(self, story.get_title())
 
 
 class SubjectTrigger(WordTrigger):
     def evaluate(self, story):
-        return WordTrigger.isWordIn(self, story.get_subject())
+        return WordTrigger.is_word_in(self, story.get_subject())
 
 
 class SummaryTrigger(WordTrigger):
     def evaluate(self, story):
-        return WordTrigger.isWordIn(self, story.get_summary())
+        return WordTrigger.is_word_in(self, story.get_summary())
 
 
 # Composite Triggers
@@ -165,7 +165,7 @@ class PhraseTrigger(Trigger):
         is_phrase_in_title = self.phrase in story.get_title()
         is_phrase_in_subject = self.phrase in story.get_subject()
         is_phrase_in_summary = self.phrase in story.get_summary()
-        return is_phrase_in_title or is_phrase_in_subject  or is_phrase_in_summary
+        return is_phrase_in_title or is_phrase_in_subject or is_phrase_in_summary
 
 
 # ======================
@@ -173,15 +173,21 @@ class PhraseTrigger(Trigger):
 # Filtering
 # ======================
 
-def filter_stories(stories, triggerlist):
+def filter_stories(stories, trigger_list):
     """
-    Takes in a list of NewsStory instances.
+    :param trigger_list: a list of triggers
+    :param stories: a list of NewsStory instances.
 
-    Returns: a list of only the stories for which a trigger in triggerlist fires.
+    Returns: a list of only the stories for which a trigger in trigger_list fires.
     """
-    # TODO: Problem 10
-    # This is a placeholder (we're just returning all the stories, with no filtering) 
-    return stories
+    filtered_stories = []
+
+    for story in stories:
+        for trigger in trigger_list:
+            if trigger.evaluate(story) and story not in filtered_stories:
+                filtered_stories.append(story)
+
+    return filtered_stories
 
 
 # ======================
@@ -189,67 +195,80 @@ def filter_stories(stories, triggerlist):
 # User-Specified Triggers
 # ======================
 
-def make_trigger(triggerMap, triggerType, params, name):
+def make_trigger(trigger_map, trigger_type, parameters, name):
     """
     Takes in a map of names to trigger instance, the type of trigger to make,
     and the list of parameters to the constructor, and adds a new trigger
     to the trigger map dictionary.
 
-    triggerMap: dictionary with names as keys (strings) and triggers as values
-    triggerType: string indicating the type of trigger to make (ex: "TITLE")
-    params: list of strings with the inputs to the trigger constructor (ex: ["world"])
-    name: a string representing the name of the new trigger (ex: "t1")
+    :param trigger_map: dictionary with names as keys (strings) and triggers as values
+    :param trigger_type: string indicating the type of trigger to make (ex: "TITLE")
+    :param parameters: list of strings with the inputs to the trigger constructor (ex: ["world"])
+    :param name: a string representing the name of the new trigger (ex: "t1")
 
-    Modifies triggerMap, adding a new key-value pair for this trigger.
+    Modifies trigger_map, adding a new key-value pair for this trigger.
 
-    Returns a new instance of a trigger (ex: TitleTrigger, AndTrigger).
+    :returns a new instance of a trigger (ex: TitleTrigger, AndTrigger).
     """
-    # TODO: Problem 11
+    trigger = None
+    if trigger_type == "TITLE":
+        trigger = TitleTrigger(parameters[0])
+    elif trigger_type == "SUBJECT":
+        trigger = SubjectTrigger(parameters[0])
+    elif trigger_type == "SUMMARY":
+        trigger = SummaryTrigger(parameters[0])
+    elif trigger_type == "NOT":
+        trigger = NotTrigger(trigger_map[parameters[0]])
+    elif trigger_type == "AND":
+        trigger = AndTrigger(trigger_map[parameters[0]], trigger_map[parameters[1]])
+    elif trigger_type == "OR":
+        trigger = OrTrigger(trigger_map[parameters[0]], trigger_map[parameters[1]])
+    elif trigger_type == "PHRASE":
+        trigger = PhraseTrigger(" ".join(parameters))
+
+    trigger_map[name] = trigger
+    return trigger
 
 
-def read_trigger_config(filename):
+def read_trigger_config(file_name):
     """
-    Returns a list of trigger objects
-    that correspond to the rules set
-    in the file filename
+    :param file_name: a text file that contains information about triggers
+    :returns a list of trigger objects that correspond to the rules set in the file file_name
     """
 
     # Here's some code that we give you
     # to read in the file and eliminate
     # blank lines and comments
-    triggerfile = open(filename, "r")
-    all = [line.rstrip() for line in triggerfile.readlines()]
+    trigger_file = open(file_name, "r")
+    all_lines = [line.rstrip() for line in trigger_file.readlines()]
     lines = []
-    for line in all:
+    for line in all_lines:
         if len(line) == 0 or line[0] == '#':
             continue
         lines.append(line)
 
     triggers = []
-    triggerMap = {}
+    trigger_map = {}
 
     # Be sure you understand this code - we've written it for you,
     # but it's code you should be able to write yourself
     for line in lines:
-
         linesplit = line.split(" ")
 
         # Making a new trigger
         if linesplit[0] != "ADD":
-            trigger = make_trigger(triggerMap, linesplit[1],
-                                   linesplit[2:], linesplit[0])
-
+            make_trigger(trigger_map, linesplit[1], linesplit[2:], linesplit[0])
         # Add the triggers to the list
         else:
             for name in linesplit[1:]:
-                triggers.append(triggerMap[name])
+                triggers.append(trigger_map[name])
 
     return triggers
 
 
 import thread
 
-SLEEPTIME = 60  # seconds -- how often we poll
+SLEEP_TIME = 60  # seconds -- how often we poll
 
 
 def main_thread(master):
@@ -261,11 +280,9 @@ def main_thread(master):
         t2 = SubjectTrigger("Romney")
         t3 = PhraseTrigger("Election")
         t4 = OrTrigger(t2, t3)
-        triggerlist = [t1, t4]
-
-        # TODO: Problem 11
-        # After implementing make_trigger, uncomment the line below:
-        # triggerlist = read_trigger_config("triggers.txt")
+        trigger_list = [t1, t4]
+        # After implementing makeTrigger, uncomment the line below:
+        trigger_list = read_trigger_config("triggers.txt")
 
         # **** from here down is about drawing ****
         frame = Frame(master)
@@ -285,15 +302,15 @@ def main_thread(master):
         button.pack(side=BOTTOM)
 
         # Gather stories
-        guidShown = []
+        guid_shown = []
 
         def get_cont(newstory):
-            if newstory.get_guid() not in guidShown:
+            if newstory.get_guid() not in guid_shown:
                 cont.insert(END, newstory.get_title() + "\n", "title")
                 cont.insert(END, "\n---------------------------------------------------------------\n", "title")
                 cont.insert(END, newstory.get_summary())
                 cont.insert(END, "\n*********************************************************************\n", "title")
-                guidShown.append(newstory.get_guid())
+                guid_shown.append(newstory.get_guid())
 
         while True:
             print "Polling . . .",
@@ -304,13 +321,13 @@ def main_thread(master):
             stories.extend(process("http://rss.news.yahoo.com/rss/topstories"))
 
             # Process the stories
-            stories = filter_stories(stories, triggerlist)
+            stories = filter_stories(stories, trigger_list)
 
             map(get_cont, stories)
             scrollbar.config(command=cont.yview)
 
             print "Sleeping..."
-            time.sleep(SLEEPTIME)
+            time.sleep(SLEEP_TIME)
 
     except Exception as e:
         print e
