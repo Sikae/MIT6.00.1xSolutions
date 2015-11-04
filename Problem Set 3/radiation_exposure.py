5421867cafb3a3c27c60a6003abe0216093068ab
M <lmiguelvargasf@gmail.com>
2015-11-03 21:26:14 -0500

    Modify pset3-radiation docstring.

--- Problem Set 3/radiation_exposure.py
+++ Problem Set 3/radiation_exposure.py
@@ -1,28 +1,7 @@
 __author__ = 'm'
 
 """
-In this problem, you are asked to find the amount of radiation a person is exposed to during some period of time by
-completing the following function:
-
-def radiationExposure(start, stop, step):
-    '''
-    Computes and returns the amount of radiation exposed
-    to between the start and stop times. Calls the
-    function f (defined for you in the grading script)
-    to obtain the value of the function at any point.
-
-    start: integer, the time at which exposure begins
-    stop: integer, the time at which exposure ends
-    step: float, the width of each rectangle. You can assume that
-      the step size will always partition the space evenly.
-
-    returns: float, the amount of radiation exposed to
-      between start and stop times.
-    '''
-
-To complete this function you'll need to know what the value of the radioactive decay curve is at various points.
-There is a function f that will be defined for you that you can call from within your function that describes the
-radioactive decay curve for the problem. Do not define f in your code.
+This program finds the amount of radiation a person is exposed to during some period of time.
 """
 
 
