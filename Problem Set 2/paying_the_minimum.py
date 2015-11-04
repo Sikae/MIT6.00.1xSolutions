8d5b49e4fb757e3a00dbc466700c09f860c328de
M <lmiguelvargasf@gmail.com>
2015-11-03 21:25:20 -0500

    Modify pset7 docstrings.

--- Problem Set 2/paying_the_minimum.py
+++ Problem Set 2/paying_the_minimum.py
@@ -1,31 +1,8 @@
 __author__ = 'm'
 
 """
-Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment
+This program calculates the credit card balance after one year if a person only pays the minimum monthly payment
 required by the credit card company each month.
-
-The following variables contain values as described below:
-
-balance - the outstanding balance on the credit card
-annualInterestRate - annual interest rate as a decimal
-monthlyPaymentRate - minimum monthly payment rate as a decimal
-
-For each month, calculate statements on the monthly payment and remaining balance, and print to screen something of
-the format:
-
-Month: 1
-Minimum monthly payment: 96.0
-Remaining balance: 4784.0
-Be sure to print out no more than two decimal digits of accuracy - so print
-
-Remaining balance: 813.41
-instead of
-
-Remaining balance: 813.4141998135
-Finally, print out the total amount paid that year and the remaining balance at the end of the year in the format:
-
-Total paid: 96.0
-Remaining balance: 4784.0
 """
 
 
